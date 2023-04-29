6de4f53efbb338adb1fe5916dd7a2a2378522f91
Todor <t.vlasev@gmail.com>
2023-04-29 14:09:28 +0300

    create dashboard page, login page and sign in and sign out func

---
+++ src/components/CopyButton.tsx
@@ -0,0 +1,39 @@
+"use client";
+
+import { cn } from "@/lib/utils";
+import { Copy } from "lucide-react";
+import { ButtonHTMLAttributes, FC } from "react";
+import Button from "./common/Button";
+import { ToastComponent } from "@/common/Toast";
+
+interface CopyButtonProps extends ButtonHTMLAttributes<HTMLButtonElement> {
+  valueToCopy: string;
+}
+
+const CopyButton: FC<CopyButtonProps> = ({
+  valueToCopy,
+  className,
+  ...props
+}) => {
+  return (
+    <Button
+      {...props}
+      type="button"
+      onClick={() => {
+        navigator.clipboard.writeText(valueToCopy);
+
+        ToastComponent({
+          title: "Copied",
+          message: "API key copied to clipboard",
+          type: "success",
+        });
+      }}
+      variant="ghost"
+      className={cn("", className)}
+    >
+      <Copy className="h-5 w-5" />
+    </Button>
+  );
+};
+
+export default CopyButton;
